ab47b6b5bb3489842312602a515fffc49729bb6d
Emily Dolson <EmilyLDolson@gmail.com>
2015-12-01 04:25:01 -0500

    typo

--- code/main09.js
+++ code/main09.js
@@ -115,7 +115,7 @@ d3.tsv("http://emilydolson.github.io/D3-visualising-data/resources/nations.csv",
 	// update the plot, includes enter, exit, and transition
   function update() {
     
-      var circle = data_canvas.selectAll("circle").data(filtered_nations, function(d){return d.country});
+      var circles = data_canvas.selectAll("circle").data(filtered_nations, function(d){return d.country});
 
       circles.enter().append("circle").attr("class","data_point")
           .style("fill", function(d) { return colorScale(d.continent); })      
